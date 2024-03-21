2c8612703319ae094c2e98e1db647eff1a746840
Cut0x <71967731+Cut0x@users.noreply.github.com>
2024-03-21 14:00:39 +0100

    add packages add structur

---
+++ main.js
@@ -0,0 +1,17 @@
+const { BotClient } = require('simple-djs-handler');
+const { GatewayIntentBits } = require('discord.js');
+
+const config = require("./src/config");
+
+const client = new BotClient({
+    token: config.bot.token,
+    slashCommandsEnabled: true,
+    slashCommandsClientId: config.bot.client_id,
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages
+    ],
+});
+
+client.start();
