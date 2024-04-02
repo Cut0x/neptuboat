bf3db82db6406e16e916fbf3cbd0e1f6792ca0ae
Cut0x <71967731+Cut0x@users.noreply.github.com>
2024-04-02 02:43:46 +0200

    update

--- commands/vote.js
+++ commands/vote.js
@@ -1,6 +1,8 @@
+// importation des modules pour le fonctionnement du bot
 const { BotCommand, CreateEmbed } = require('simple-djs-handler');
 const wait = require('node:timers/promises').setTimeout;
 
+// création de la structure de la commande
 module.exports = new BotCommand({
     name: 'vote',
     description: '\ud83c\udfd3 Te permet de voter pour le bot !',
@@ -9,11 +11,13 @@ module.exports = new BotCommand({
 		
         await wait(1000);
 
+        // création d'un embred
         const ping_embed = new CreateEmbed({
             color: 0x996bff,
             description: `Pour voter, tu peux [cliquer ici](https://akiras.xyz/vote/?id=${interaction.client.id}) ou cliquer sur le lien ci-dessous :\n\n> :link: https://akiras.xyz/vote/?id=1220081470995959869`
         })
 
+        // réponse du bot avec l'embed
         await interaction.editReply({
 			content: `## <:pepeok:1020389675841622057> Merci`,
 			embeds: [
--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,14 @@
+// importation des modules pour le fonctionnement du bot
 const { BotEvent } = require('simple-djs-handler');
 const { Events, ActivityType } = require('discord.js');
 
 const { bot } = require("../src/config");
 
+// construction de la structure de l'évènement
 module.exports = new BotEvent({
     name: Events.ClientReady,
     async execute(c) {
+        // définition de l'activité du bot
         c.user.setPresence({
             activities: [
                 {
